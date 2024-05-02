124a1907f8d537600c5f8cf2860a2cdd0acd6e15
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2024-05-02 19:27:09 +0000

    object state and spread the previous state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 
-function countInitial() {
-  console.log("run function");
-  return 4;
-}
-
 function App() {
-  const [count, setCount] = useState(() => countInitial());
+  const [state, setState] = useState({ count: 4, theme: "blue" });
+  const count = state.count;
+  const theme = state.theme;
 
   function decrementCount() {
-    setCount((prevCount) => prevCount - 1);
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count - 1 };
+    });
   }
   function incrementCount() {
-    setCount((prevCount) => prevCount + 1);
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count + 1 };
+    });
   }
 
   return (
     <>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={incrementCount}>+</button>
     </>
   );
